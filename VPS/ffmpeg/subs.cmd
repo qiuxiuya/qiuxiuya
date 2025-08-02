bd4ac4da0947d48102821ae8b66b3bb26640a6bd
qiuxiuya <95994462+qiuxiuya@users.noreply.github.com>
2025-08-02 20:45:57 +0800

    Create subs.cmd

---
+++ VPS/ffmpeg/subs.cmd
@@ -0,0 +1,54 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set /p video_dir=请输入视频文件所在目录（例如 D:\videos）:
+set /p subs_dir=请输入字幕文件所在目录（例如 D:\subs）:
+set /p subs_ext=请输入字幕文件后缀（例如 ass）:
+set /p out_dir=请输入输出目录（例如 D:\output）:
+
+if not exist "%out_dir%" (
+    mkdir "%out_dir%"
+)
+
+for %%F in ("%video_dir%\*") do (
+    if exist "%%F" (
+        set "filename=%%~nxF"
+        setlocal enabledelayedexpansion
+        set "base_name=!filename:~0,-4!"
+
+        set "subtitle_file=%subs_dir%\!base_name!.%subs_ext%"
+
+        if exist "!subtitle_file!" (
+            call :NormalizePath "%%F" video_path_norm
+            call :NormalizeSubtitlePath "!subtitle_file!" subtitle_path_norm
+            call :NormalizePath "%out_dir%\!base_name!.mp4" out_file_norm
+
+            ffmpeg.exe -i "!video_path_norm!" -vf subtitles="!subtitle_path_norm!" -c:v h264_nvenc -crf 18 -c:a copy "!out_file_norm!"
+        ) else (
+            echo 字幕 !subtitle_file! 不存在
+        )
+        endlocal
+    )
+)
+goto :eof
+
+:NormalizePath
+setlocal enabledelayedexpansion
+set "p=%~1"
+set "p=!p:\=\/!"
+endlocal & set "%2=%p%"
+goto :eof
+
+:NormalizeSubtitlePath
+setlocal enabledelayedexpansion
+set "p=%~1"
+set "p=!p:\=\/!"
+
+if "!p:~1,1!"==":" (
+    set "p=!p:~0,1!\\:!p:~2!"
+)
+
+set "p=!p:[=\[!"
+set "p=!p:]=\]!"
+endlocal & set "%2=%p%"
+goto :eof
