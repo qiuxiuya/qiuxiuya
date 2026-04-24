c6e4878102110df9000979b23dcb6a2081d08623
qiuxiuya <ds2233039480@gmail.com>
2026-04-24 18:00:59 +0800

    233

--- VPS/Sub-StoreJS/mux.js
+++ VPS/Sub-StoreJS/mux.js
@@ -1,7 +1,7 @@
-if ($server['type'] !== 'anytls') {
-    $server.smux = {
+if (($server['type'] !== 'anytls' && $server['type'] !== 'ss') || ($server['type'] === 'ss' && ($server['sni'] || $server['plugin'] === 'shadow-tls'))) {
+  $server.smux = {
     "enabled": true,
-      "protocol": "h2mux",
-      "padding": true
-    };
+    "protocol": "h2mux",
+    "padding": true
+  };
 }
