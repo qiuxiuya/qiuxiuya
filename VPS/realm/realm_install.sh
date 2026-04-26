e4d7764c21597147932a43b1c1bfc9e4602e1b2d
qiuxiuya <ds2233039480@gmail.com>
2026-04-26 20:28:47 +0800

    233

--- VPS/realm/realm_install.sh
+++ VPS/realm/realm_install.sh
@@ -30,12 +30,30 @@ fi
 
 wget -q -O /etc/realm/config.toml https://raw.githubusercontent.com/qiuxiuya/qiuxiuya/refs/heads/main/VPS/realm/config.toml
 
-wget -q -O /etc/systemd/system/${SERVICE_NAME}.service https://github.com/qiuxiuya/qiuxiuya/raw/refs/heads/main/VPS/realm/realm.service
+cat > /etc/systemd/system/${SERVICE_NAME}.service <<EOF
+[Unit]
+Description=realm
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/etc/realm
+ExecStart=/etc/realm/realm -c /etc/realm/config.toml
+Restart=on-failure
+RestartSec=5s
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 chmod +x /etc/realm/realm
 
 systemctl daemon-reload
-
 systemctl enable ${SERVICE_NAME}
 
 echo -e "realm installed successfully.\nPlease \033[33mnano /etc/realm/config.toml\033[0m to change your config.\nAnd \033[33msystemctl start ${SERVICE_NAME}\033[0m to start realm server."
